dcb166be6f2ec9b300b42a39c3de37472ab411e1
Júlia Cardozo <ccardozojulia@gmail.com>
2022-11-27 16:23:34 -0300

    TODO 21

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -72,6 +72,4 @@ class StoryBrain {
 
 //TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
 
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
