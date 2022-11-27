2c84cd3cbcc233cc8c906f7da72362b5fb1aadfa
Júlia Cardozo <ccardozojulia@gmail.com>
2022-11-27 14:45:45 -0300

    first commit

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story {
+  late String storyTitle;
+  late String choice1;
+  late String choice2;
+
+  Story(
+      {required String storyTitle,
+      required String choice1,
+      required String choice2});
+}
